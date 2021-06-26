3b2d4daf21d657f81147c1fc72b697af18183681
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-06-26 14:26:14 +0700

    upadate app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import './asset/css/style.css';
 import HomePage from './Pages/HomePage';
+
 function App() {
-  return (
-    <div className="App">
-     <HomePage/>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        div > hello < /div> <
+        HomePage / >
+        <
+        /div>
+    );
 }
 
 export default App;
--- src/Auth/index.js
+++ src/Auth/index.js
@@ -2,10 +2,10 @@ import axiosClient from "../api/axiosClient";
 
 
 const AuthAPI = {
-    login(email,password){
+    login(email, password) {
         const url = `/signin?_email=${email}&_password=${password}`
         return axiosClient.post(url)
-    }    
+    }
 }
 
 export default AuthAPI;
