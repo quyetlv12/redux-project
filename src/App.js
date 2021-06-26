27cd02ca6f1b3c056618836f282a6bac57a1d3c6
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-06-26 14:35:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import './asset/css/style.css';
-import HomePage from './Pages/HomePage';
+import "./App.css";
+import "./asset/css/style.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        div > hello < /div> <
-        HomePage / >
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+    <div></div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
